962e49e549a6a19240d1e7d9a2b73d1e9a1e7b7f
phantom0109 <phantom0109@gmail.com>
2021-05-06 08:54:38 +0800

    verifying the initial message

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -6,6 +6,7 @@ const { interface, bytecode } = require('../compile')
 
 let accounts
 let inbox
+const INITIAL_STRING = 'Hi there!'
 
 beforeEach(async () => {
     // Get a list of all accounts
@@ -13,12 +14,20 @@ beforeEach(async () => {
 
     // Use one of those accounts to deploy the contract
     inbox = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({ data: bytecode, arguments: ['Hi there!'] })
+        .deploy({ 
+            data: bytecode, 
+            arguments: [INITIAL_STRING] 
+        })
         .send({ from: accounts[0], gas: '1000000'  })
 })
 
 describe('Inbox', () => {
     it('deploy a contract', () => {
         assert.ok(inbox.options.address)
+    })
+
+    it('has a default message', async () => {
+        const message = await inbox.methods.message().call()
+        assert.equal(message, INITIAL_STRING)
     })
 })
